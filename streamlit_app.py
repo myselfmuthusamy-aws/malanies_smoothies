503e9744ae2f9b5c164dc6775828f32049f835d9
myselfmuthusamy-aws <myselfmuthusamy.aws@gmail.com>
2026-08-15 15:26:31 +0530

    Display nutrition information for selected fruits
    Added nutrition information display for selected fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,13 @@ if ingredients_list:
     ingredients_string = ""
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.write(ingredients_string)
+
+
+    
 
 
 
@@ -66,10 +72,3 @@ values ('{ingredients_string}', '{name_on_order}')
 # st.text(smoothiefroot_response).json()
 
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-
-# st.text(smoothiefroot_response.text).json()
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
