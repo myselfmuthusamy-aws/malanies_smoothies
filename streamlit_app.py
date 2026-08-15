6f65339c3ac7c41bc91b270a61d174179928debc
myselfmuthusamy-aws <myselfmuthusamy.aws@gmail.com>
2026-08-15 14:23:55 +0530

    Fetch and display watermelon data from API
    Added a request to fetch watermelon data from SmoothieFroot API and display the response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,6 @@ values ('{ingredients_string}', '{name_on_order}')
 # st.write(my_insert_stmt)
 # st.dataframe(data = my_dataframe, use_container_width = True)
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
